1441a5388409bb8075b55d7430ead62bf2c3071e
Your Name <you@example.com>
2022-02-07 22:33:33 +0100

    showQuestionsFunktion

---
+++ script.js
@@ -0,0 +1,63 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    },
+    {
+        "question": "was bedeutet das HTML Tag &lt;a&gt",
+        "answer_1": "Text Fett",
+        "answer_2": "Container",
+        "answer_3": "Ein Link",
+        "answer_4": "Kursiv",
+        "right_answer": 3
+    },
+    {
+        "question": "wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    },
+    {
+        "question": "Wie bindet man eine Website in eine Website ein?",
+        "answer_1": "&lt;iframe&gt;, &lt;frame&gt;, and &lt;frameset&gt;",
+        "answer_2": "&lt;iframe&gt;",
+        "answer_3": "&lt:frame&gt;",
+        "answer_4": "&lt;frameset&gt",
+        "right_answer": 2
+    },
+    {
+        "question": "In welchem Jahr wurde JavaScribt erfunden?",
+        "answer_1": "1995",
+        "answer_2": "2002",
+        "answer_3": "1978",
+        "answer_4": "1987",
+        "right_answer": 1
+    },
+
+];
+
+let currentQuestion = 0;
+
+function init() {
+    document.getElementById('all_questions').innerHTML = questions.length;
+    showQuestion();
+}
+
+function showQuestion() {
+    let question = questions[currentQuestion];
+    document.getElementById('question_text').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+}
+
+function answer(selection) {
+    
+}
