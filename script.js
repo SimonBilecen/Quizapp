a5e8cb9bf2cd48f466bcdcb626078b58cfdee64f
Your Name <you@example.com>
2022-02-09 20:52:57 +0100

    New design

--- script.js
+++ script.js
@@ -16,15 +16,7 @@ let questions = [
         "right_answer": 3
     },
     {
-        "question": "wer hat HTML erfunden?",
-        "answer_1": "Robbie Williams",
-        "answer_2": "Lady Gaga",
-        "answer_3": "Tim Berners-Lee",
-        "answer_4": "Justin Bieber",
-        "right_answer": 3
-    },
-    {
-        "question": "Wie bindet man eine Website in eine Website ein?",
+        "question": "Wie bindet man eine Website ein?",
         "answer_1": "&lt;iframe&gt;, &lt;frame&gt;, and &lt;frameset&gt;",
         "answer_2": "&lt;iframe&gt;",
         "answer_3": "&lt:frame&gt;",
@@ -32,11 +24,11 @@ let questions = [
         "right_answer": 2
     },
     {
-        "question": "In welchem Jahr wurde JavaScribt erfunden?",
-        "answer_1": "1995",
-        "answer_2": "2002",
-        "answer_3": "1978",
-        "answer_4": "1987",
+        "question": "Was ist HTML?",
+        "answer_1": "Auszeichnungssprache für Hypertext?",
+        "answer_2": "Älteste Programmiersprache",
+        "answer_3": "Der Vorgänger von php",
+        "answer_4": "Eine möglichkeit seinen Code zu stylen",
         "right_answer": 1
     },
 
@@ -50,24 +42,70 @@ function init() {
 }
 
 function showQuestion() {
-    let question = questions[currentQuestion];
-    document.getElementById('question_text').innerHTML = question['question'];
-    document.getElementById('answer_1').innerHTML = question['answer_1'];
-    document.getElementById('answer_2').innerHTML = question['answer_2'];
-    document.getElementById('answer_3').innerHTML = question['answer_3'];
-    document.getElementById('answer_4').innerHTML = question['answer_4'];
+    if (currentQuestion >= questions.length) {
+        // Todo
+        document.getElementById('end_screen').style =  '';
+        document.getElementById('question_body').style = "display:none";
+    }
+    else {
+        current_quiz_topic();
+
+        let question = questions[currentQuestion];
+        document.getElementById('current_question_number').innerHTML = currentQuestion + 1;
+        document.getElementById('question_text').innerHTML = question['question'];
+        document.getElementById('answer_1').innerHTML = question['answer_1'];
+        document.getElementById('answer_2').innerHTML = question['answer_2'];
+        document.getElementById('answer_3').innerHTML = question['answer_3'];
+        document.getElementById('answer_4').innerHTML = question['answer_4'];
+    }
 }
 
 function answer(selection) {
     let question = questions[currentQuestion];
     let selectedNumber = selection.slice(-1);
+    let idOfRightAnswer = `answer_${question['right_answer']}`;
 
     if (selectedNumber == question['right_answer']) {
         console.log('passt');
-        document.getElementById(selection).classList.add('bg-success');
+        document.getElementById(selection).parentNode.classList.add('bg-success');
     }
     else {
         console.log('passt nicht du depp.')
         document.getElementById(selection).parentNode.classList.add('bg-danger');
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+    }
+
+    document.getElementById('next-button').disabled = false;
+}
+
+function next_question() {
+
+    currentQuestion++;
+    showQuestion();
+    resetAnswerButtons();
+
+    if (questions.length -1 == currentQuestion){
+        document.getElementById('next-button').innerHTML = "Ergebnis anzeigen";
+    }
+}
+
+function resetAnswerButtons() {
+    document.getElementById('next-button').disabled = true;
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+}
+
+function current_quiz_topic() {
+    if(currentQuestion <=3 ) {
+        document.getElementById('first_topic').classList.add('current_quiz2');
+    }
+    else {
+        document.getElementById('first_topic').classList.remove('current_quiz2');
     }
 }
